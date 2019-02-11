b7a1865f1c1b521122e604a5bf2be304f6d4fb7a
Tamara Marcetic <tamaramarcetic@hotmail.com>
2019-02-11 20:06:07 +0100

    Changes to be committed: 	new file:   1_Iterations/1_BinaryGap.c

---
+++ 1_Iterations/1_BinaryGap.c
@@ -0,0 +1,24 @@
+int solution(int N) {
+
+    int max = 0, counter = 0;
+    N = (unsigned) N;
+    
+    while(N > 0){
+        if(N == 1)
+            break;
+        counter = 0;
+        if(N&1){
+            N >>= 1;
+            while((N&1) == 0){
+                counter++;
+                N >>= 1;
+            }
+        }
+        else
+            N >>= 1;
+        if(counter > max)
+            max = counter;
+    }
+    
+    return max;
+}
